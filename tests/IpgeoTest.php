41f30486027c69e1d411bf52a1892e4246f899ad
Kirill Grafov <countjr@gmail.com>
2019-02-08 23:04:21 +0100

    Add first test

--- tests/IpgeoTest.php
+++ tests/IpgeoTest.php
@@ -2,12 +2,16 @@
 
 namespace Countjr\Ipgeo\Tests;
 
+use Countjr\Ipgeo\Ipgeo;
+use Countjr\Ipgeo\Services\Ipapi;
 use PHPUnit\Framework\TestCase;
 
 class IpgeoTest extends TestCase
 {
-    public function testIpgeoHomeIp()
+    public function testIpapi()
     {
-        //
+        $client = new Ipapi();
+        $ipgeo = new Ipgeo($client);
+        $this->assertEquals(file_get_contents(__DIR__ . '/asserts/134.234.3.2'), $ipgeo->getLocation('134.234.3.2'));
     }
 }
